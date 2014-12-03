4f34823e4a9202eb85c14bdb95c1d9e86fd1726c
aptxyz <aptxyz@gmail.com>
2014-12-02 22:21:12 -0800

    fixed something

--- README.md
+++ README.md
@@ -1,37 +1,40 @@
-IPMON
-=====
+## IPMON
 
 IPMON monitors your router's bandwidth.
 
 ![](https://github.com/aptxyz/ipmon/blob/master/snapshot.png)
 
-Installation
-=====
+## Installation
 
 1. Make sure your server has mySQL and php
-2. Make sure your router has jffs enable.
-3. Put index.php and index.htm on your server.
-4. Put ipmon.startup on your router of /jffs/etc/config/
+2. Make sure your router has jffs enable
+3. Put `index.php` and `index.htm` on your server
+4. Put `ipmon.wanup` on your router of `/jffs/etc/config/`
 5. Change your variables
- * $routerIP, $resetD on index.php
- * ipmon.resetD on index.htm
- * serverIP, serverPath on ipmon.startup
-6. Run `./ipmon.startup server-setup` on your server, enter your mySQL root password
-7. Run `./ipmon.startup router-setup` on your router
-8. Reboot your router or run `./ipmon.startup start &` on your router
-9. Open your browser
+ * $routerIP on `index.php`
+ * ipmon.resetD on `index.htm`
+ * serverIP, serverPath on `ipmon.wanup`
+6. Run `./ipmon.wanup server-setup` on your server, enter your mySQL root password
+7. Run `./ipmon.wanup &` on your router
+8. Open your browser
 
 ![](https://github.com/aptxyz/ipmon/blob/master/snapshot2.png)
 
-Version
-=====
+## Version
 
-0.01
+* v0.02 (2014-12-02) - fixed something
+* v0.01 (2014-08-11) - initial commit
 
-Credit
-=====
+## Credit
 
 - Based on work from Fredrik Erlandsson (erlis AT linux.nu)
 - Based on traff_graph script by twist - http://wiki.openwrt.org/RrdTrafficWatch
 - Based on wrtbwmon by Emmanuel Brucy (e.brucy AT qut.edu.au)i - https://code.google.com/p/wrtbwmon/
 - Based on lal-projects by Ajudica...@gmail.com - https://code.google.com/p/lal-projects/
+- Used the libraries
+ * Highcharts JS by Highsoft AS - http://www.highcharts.com/
+ * Datejs by Coolite Inc.- http://www.datejs.com/
+ * jQuery by jQuery Foundation- http://jquery.com/
+
+
+
--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
-session_start();
+if(!isset($_SESSION)) { 
+	session_start(); 
+} 
 
 $routerIP = '192.168.0.1';
-$resetD = 24; // Bandwidth Cycle Start on # of Every Month
 
 function connect() {
-	$mysqli = new mysqli('127.0.0.1','IPMON','uNuKV3F7HHfTEu4T','IPMON');
+	$mysqli = new mysqli('localhost','IPMON','uNuKV3F7HHfTEu4T','IPMON');
 	if (mysqli_connect_errno()) {
 		printf("Connect failed: %s\n", mysqli_connect_error());
 		exit();
@@ -64,19 +65,19 @@ if (isset($_GET['insert']) && $_SERVER['REMOTE_ADDR'] == $routerIP) {
 		$stmt->bind_param('ssiiii', $macid, $curDay, $up, $down, $up, $down);
 		$stmt->execute();
 		
-		// mac: month
-		$stmt = $mysqli->prepare("
-			INSERT INTO monthly (macid, date, up, down)
-			VALUES (?, ?, ?, ?)
-			ON DUPLICATE KEY UPDATE up=up+?,down=down+?
-		");
-		$stmt->bind_param('ssiiii', $macid, $curMonth, $up, $down, $up, $down);
-		$stmt->execute();
+		// // mac: month
+		// $stmt = $mysqli->prepare("
+			// INSERT INTO monthly (macid, date, up, down)
+			// VALUES (?, ?, ?, ?)
+			// ON DUPLICATE KEY UPDATE up=up+?,down=down+?
+		// ");
+		// $stmt->bind_param('ssiiii', $macid, $curMonth, $up, $down, $up, $down);
+		// $stmt->execute();
 		
 		// mac: total
 		$stmt = $mysqli->prepare("
-			INSERT INTO monthly (macid, date, up, down)
-			VALUES (?, '0000', ?, ?)
+			INSERT INTO total (macid, up, down)
+			VALUES (?, ?, ?)
 			ON DUPLICATE KEY UPDATE up=up+?,down=down+?
 		");
 		$stmt->bind_param('siiii', $macid, $up, $down, $up, $down);
@@ -91,19 +92,19 @@ if (isset($_GET['insert']) && $_SERVER['REMOTE_ADDR'] == $routerIP) {
 		$stmt->bind_param('siiii', $curDay, $up, $down, $up, $down);
 		$stmt->execute();
 		
-		// everyone: month
-		$stmt = $mysqli->prepare("
-			INSERT INTO monthly (macid, date, up, down)
-			VALUES (0, ?, ?, ?)
-			ON DUPLICATE KEY UPDATE up=up+?,down=down+?
-		");
-		$stmt->bind_param('siiii', $curMonth, $up, $down, $up, $down);
-		$stmt->execute();
+		// // everyone: month
+		// $stmt = $mysqli->prepare("
+			// INSERT INTO monthly (macid, date, up, down)
+			// VALUES (0, ?, ?, ?)
+			// ON DUPLICATE KEY UPDATE up=up+?,down=down+?
+		// ");
+		// $stmt->bind_param('siiii', $curMonth, $up, $down, $up, $down);
+		// $stmt->execute();
 		
 		// everyone: total
 		$stmt = $mysqli->prepare("
-			INSERT INTO monthly (macid, date, up, down)
-			VALUES (0, '0000', ?, ?)
+			INSERT INTO total (macid, up, down)
+			VALUES (0, ?, ?)
 			ON DUPLICATE KEY UPDATE up=up+?,down=down+?
 		");
 		$stmt->bind_param('iiii', $up, $down, $up, $down);
@@ -118,10 +119,11 @@ elseif  (isset($_GET['select'])){
 
 	$mysqli = connect();
 	$select = $_GET['select'];
-	$i = $_GET['i'];
-	$f = $_GET['f'];
 
-	if ($select == 'daily' || $select == 'monthly') {
+	if ($select == 'month') {
+    
+        $i = $_GET['i'];
+        $f = $_GET['f'];
 	
 		$mac = array();
 		$result = $mysqli->query("SELECT mac, ip, name FROM mac ORDER BY id");
@@ -130,7 +132,7 @@ elseif  (isset($_GET['select'])){
 	
 		$stmt = $mysqli->prepare("
 			SELECT  `date` ,  `macid` ,  `up` ,  `down` 
-			FROM  `$select` 
+			FROM  `daily` 
 			WHERE  `date` BETWEEN  ? AND  ?
 			ORDER BY  `date` ,  `macid` ASC
 			LIMIT 0 , 1000
@@ -141,18 +143,54 @@ elseif  (isset($_GET['select'])){
 		while ($stmt->fetch()) {
 			$data[$date][] = array($macid, $up, $down);
 		};
+        
+		
+		echo json_encode(		array( "mac" => $mac, "data" => $data)		);
+		
+	}
+	elseif ($select == 'year') {
+    
+        $dates = $_GET['dates'];
+	
+		$mac = array();
+		$result = $mysqli->query("SELECT mac, ip, name FROM mac ORDER BY id");
+		while ($row = $result->fetch_array(MYSQLI_NUM)) {$mac[] = $row;}
+		$mac = json_encode($mac);
+        
+        $data = [];
+        
+        for ($j = 0; $j < count($dates); $j++) {
+            $month = $dates[$j][0];
+            $i = $dates[$j][1];
+            $f = $dates[$j][2];
+            $stmt = $mysqli->prepare("
+                SELECT  `macid` ,  sum(`up`) ,  sum(`down`)
+                FROM  `daily` 
+                WHERE  `date` BETWEEN  ? AND  ?
+                GROUP BY `macid`
+                ORDER BY  `date` ,  `macid` ASC
+                LIMIT 0 , 1000
+            ");
+            $stmt->bind_param('ss', $i, $f);
+            $stmt->execute();
+            $stmt->bind_result($macid, $up, $down);
+            $data[$month] = [];
+            while ($stmt->fetch()) {
+                $data[$month][] = array($macid, $up, $down);
+            };
+            
+        };
 		
 		echo json_encode(		array( "mac" => $mac, "data" => $data)		);
 		
 	}
 	elseif ($select == 'total') {
-	
+        
 		$stmt = $mysqli->prepare("
 			SELECT  `mac` ,  `ip` ,  `name` ,  `up` ,  `down` 
-			FROM  `monthly` 
+			FROM  `total` 
 			LEFT JOIN  `mac` ON  `macid` =  `id` 
-			WHERE  `date` =  '0000'
-			ORDER BY  `down` DESC 
+			ORDER BY  (`up`+`down`) DESC 
 			LIMIT 0 , 1000
 		");
 		$stmt->execute();
